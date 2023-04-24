f8b76dadf1e1670ccb0b06bec8676cb45e4ed876
bilguun <bekbilgn2@gmail.com>
2023-04-24 14:47:43 +0800

    first

---
+++ lib/constants.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF432a74);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+);
+const kSecondaryColor = Color(0xFF979797);
+const kTextColor = Colors.black;
+const kTextGreyColor = Colors.black54;
+const kLambdaOrangeColor = Color(0xFFFAA73F);
+const kSplashColor = Colors.green;
+const appBarIconColor = Colors.white;
+const String masterPinCode = "2727";
+const String productOrderCounter = "1";
+const String productOrderName = "2";
+const String productOrderId = "3";
+
+const double SPACE_XS = 8.0;
+const double SPACE_SM = 12.0;
+const double SPACE_MD = 16.0;
+const double SPACE_LG = 20.0;
+
+const double FONT_XS = 8.0;
+const double FONT_SM = 14.0;
+const double FONT_MD = 18.0;
+const double FONT_LG = 24.0;
+
+const double TITLE_FONT_SM = 20.0;
+const double TITLE_FONT_MD = 22.0;
+
+const double BORDER_RADIUS_SM = 10.0;
+const double BORDER_RADIUS_MD = 15.0;
+const double BORDER_RADIUS_CIRCULAR = 50.0;
+
+const double ICON_SIZE_SM = 20.0;
+const double ICON_SIZE_MD = 30.0;
+const double ICON_SIZE_LG = 40.0;
+
+const double QR_SIZE_SM = 180.0;
+const double QR_SIZE_MD = 250.0;
+const double QR_SIZE_LG = 300.0;
+
+const double CONTAINER_HEIGHT_SM = 40.0;
+const double CONTAINER_HEIGHT_MD = 50.0;
+const double CONTAINER_HEIGHT_LG = 70.0;
+
+const double CONTAINER_WIDTH_SM = 40.0;
+const double CONTAINER_WIDTH_MD = 50.0;
---
+++ lib/widget/menu_item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+class MenuItemModel {
+  final String title;
+  final IconData icon;
+  final Function onTap;
+
+  MenuItemModel(this.title, this.icon, this.onTap);
+}
+
+class MenuItemWidget extends StatelessWidget {
+  final MenuItemModel data;
+
+  const MenuItemWidget(this.data, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      borderRadius: BorderRadius.circular(BORDER_RADIUS_MD),
+      color: kLambdaOrangeColor,
+      child: InkWell(
+        splashColor: kSplashColor,
+        splashFactory: InkRipple.splashFactory,
+        onTap: () {
+          data.onTap();
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(data.icon, size: ICON_SIZE_LG),
+            const SizedBox(height: SPACE_SM),
+            Text(
+              data.title,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: FONT_SM,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
